4ef2805bf6d0e60928bc3db99d20803a2d3c566f
Anushk Kumar <146085675+berserk-23115@users.noreply.github.com>
2024-03-29 17:46:12 +0530

    Script Phase 1 done

--- command.sh
+++ command.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+CURR_DIREC="$(pwd)"
+TEST_DIREC="TEST"
+if [ ! -d "$TEST_DIREC" ]; then
+  echo "TEST DIRECTORY NOT FOUND !"
+  echo "CREATED , CREATE INPUT FILES IN THE TEST FOLDER"
+  mkdir "$TEST_DIREC"
+  mkdir "$TEST_DIREC/INPUT"
+  mkdir "$TEST_DIREC/OUTPUT"
+  mkdir "$TEST_DIREC/SIMULATE"
+fi
+#SCRIPT TO GENERATE OUT AND SIM FILES OF INPUT FILES IN INPUT DIRECTORY
+cd "$CURR_DIREC/$TEST_DIREC/INPUT"
+
+for file in * ; do
+  RAWFILENAME=$(basename "$file" .txt)
+  cd "$CURR_DIREC/$TEST_DIREC/OUTPUT" && touch "${RAWFILENAME}_out.txt"
+  cd "$CURR_DIREC/$TEST_DIREC/SIMULATE" && touch "${RAWFILENAME}_sim.txt"
+  
+done
+
