c77b7a6cf1aa2d638096fd902884b3d00c814d16
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-04-13 19:53:56 +0530

    main function

---
+++ assembler/target/debug/deps/assembler-16d96b6a0986c489.d
@@ -0,0 +1,7 @@
+/home/ayush/Assembly-RISCV-CO2024/assembler/target/debug/deps/libassembler-16d96b6a0986c489.rmeta: src/main.rs src/helper_functions.rs src/instructions.rs
+
+/home/ayush/Assembly-RISCV-CO2024/assembler/target/debug/deps/assembler-16d96b6a0986c489.d: src/main.rs src/helper_functions.rs src/instructions.rs
+
+src/main.rs:
+src/helper_functions.rs:
+src/instructions.rs:
---
+++ assembler/target/debug/deps/assembler-a4d9548a2f0e4f9c.d
@@ -0,0 +1,7 @@
+/home/ayush/Assembly-RISCV-CO2024/assembler/target/debug/deps/libassembler-a4d9548a2f0e4f9c.rmeta: src/main.rs src/helper_functions.rs src/instructions.rs
+
+/home/ayush/Assembly-RISCV-CO2024/assembler/target/debug/deps/assembler-a4d9548a2f0e4f9c.d: src/main.rs src/helper_functions.rs src/instructions.rs
+
+src/main.rs:
+src/helper_functions.rs:
+src/instructions.rs:
